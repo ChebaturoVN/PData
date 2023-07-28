027bbcf800840de6b5090840726ca5da442d7b84
ChebaturoVN <kukuruza2488@gmail.com>
2023-07-28 19:53:06 +0300

    start-project-without-storyboard
    delete storyboard

---
+++ PData/PData/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PData
+//
+//  Created by VladimirCH on 28.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .cyan
+
+    }
+
+
+}
+
---
+++ PData/Pods/Target Support Files/Pods-PData/Pods-PData-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PData : NSObject
+@end
+@implementation PodsDummy_Pods_PData
+@end
